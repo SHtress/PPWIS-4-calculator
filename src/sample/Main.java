3615784b9041772ebbe8fab2a40461bf0fed5323
SHtress <44646393+SHtress@users.noreply.github.com>
2020-06-02 15:30:30 +0300

    Add files via upload

---
+++ src/sample/Main.java
@@ -0,0 +1,30 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        View view = new View();
+        primaryStage=view.getStage();
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
